d1cbd4b24f4610174fc7ff0ec1a7617e4b3da99c
omelchert <melchert@iqo.uni-hannover.de>
2025-04-11 12:03:04 +0200

    Updated minimal example

--- results/numExp08_minimalExample/main_minimalExample_v1.py
+++ results/numExp08_minimalExample/main_minimalExample_v1.py
@@ -2,19 +2,16 @@ import sys; sys.path.append('../../src/')
 import numpy as np
 from SWtools import SRM
 
-# -- SETUP AND INITIALIZATION 
-xi = np.linspace(-10, 10, 2**10)
-# ... NEVP INGREDIENTS 
-cL = (0., 0., 0., -2.2/24)
-#cL = (-0.07, -0.2/2, 0.2/6, -1./24)
-F = lambda I, xi: 4.07*I
-kap = 1.76
-# ... INITIAL CONDITION 
-U0 = np.exp(-xi**2)
-# ... SRM INSTANTIATION
-myS = SRM(xi, cL, F, verbose=True)
+myS = SRM(
+  np.linspace(-10, 10, 2**10),  # COMPUTATIONAL DOMAIN
+  (0, 0, 0, -0.0917),           # NEVP - COEFFS. LINEAR PART
+  lambda I, xi: 4.07*I,         # NEVP - NONLINEAR FUNCTIONAL
+  verbose = True                # NEVP - LIST DETAILS DURING ITERATION
+)
 
-# -- SRM SOLUTION PROCEDURE
-myS.solve(U0, kap)
+myS.solve(
+  np.exp(-myS.xi**2),           # SRM - INITIAL CONDITION
+  1.76                          # SRM - EIGENVALUE OF SOUGHT-FOR SOLUTION
+)
 
 myS.show('fig_minimalExample_v1.png')
