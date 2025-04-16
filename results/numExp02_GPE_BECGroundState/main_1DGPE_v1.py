4c83471b3c0f10c02e78e388acbae10da8d78260
omelchert <melchert@iqo.uni-hannover.de>
2025-04-16 14:51:04 +0200

    Updated example 01

--- results/numExp02_GPE_BECGroundState/main_1DGPE_v1.py
+++ results/numExp02_GPE_BECGroundState/main_1DGPE_v1.py
@@ -1,3 +1,20 @@
+"""main_1DGPE_v1.py
+
+This module demonstrates the calculation of nonlinear ground-state solutions of
+Bose-Einstein condensates (BECs) for a one-dimensional Gross-Pitaevskii
+equation (GPE) [1]_. This example is discussed as a SWtools use-case in [2]_.
+
+References
+----------
+
+.. [1] W. Bao, Q. Du, Computing the ground state solution of Bose-Einstein
+condensates by a normalized gradient flow, SIAM J. Sci. Comput. 25
+(2004) 1674–1697, https://doi.org/10.1137/S1064827503422956.
+
+.. [2] O. Melchert, A. Demircan, https://doi.org/10.48550/arXiv.2504.10623.
+
+.. codeauthor:: Oliver Melchert <melchert@iqo.uni-hannover.de>
+"""
 import numpy as np
 from SWtools import NSOM
 
@@ -16,7 +33,7 @@ myS = NSOM(xi, cL, F, ORP=1.5, verbose=True, nskip=10)
 # -- NSOM SOLUTION PROCEDURE
 myS.solve(U0, N0)
 
-# -- POSTPROCESSING (RMS WIDTH AND ENERGY)
+# -- POSTPROCESSING
 U, mu = myS.U, -myS.kap
 xi_rms = np.sqrt(np.trapz((xi*U)**2,x=xi))
 E = mu - 0.5*beta*np.trapz(U**4,x=xi)
@@ -24,5 +41,4 @@ print(f"max(U) = {np.max(U):5.4F}")
 print(f"xi_rms = {xi_rms:5.4F}")
 print(f"E_beta = {E:5.4F}")
 print(f"mu     = {mu:5.4F}")
-
-myS.show()
+print(myS); myS.show()
