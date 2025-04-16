d5c42ef60e7fcd7171f8cb567b6e3582133558e8
omelchert <melchert@iqo.uni-hannover.de>
2025-04-16 15:14:41 +0200

    Updated example 08

--- results/numExp08_minimalExample/main_minimalExample_v1.py
+++ results/numExp08_minimalExample/main_minimalExample_v1.py
@@ -1,3 +1,21 @@
+"""main_minimalExample_v1.py
+
+This module demonstrates the calculation of a solitary wave solution for a
+higher-order nonlinear Schrödinger equation, modeled after [1]_. This example
+is discussed as SWtools use-case at [2]_.
+
+References
+----------
+
+.. [1] Widjaja et al., Absence of Galilean invariance for pure-quartic
+solitons, Phys. Rev. A 104 (2021) 043526,
+https://doi.org/10.1103/PhysRevA.104.043526.
+
+.. [2] https://github.com/omelchert/SWtools
+
+.. codeauthor:: Oliver Melchert <melchert@iqo.uni-hannover.de>
+
+"""
 import numpy as np
 from SWtools import SRM
 
