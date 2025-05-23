ccf4e0d0cc4d1a0ec949c9d7d3bffd65f8a4d509
omelchert <melchert@iqo.uni-hannover.de>
2025-05-23 16:46:17 +0200

    Updated example 02

--- results/numExp02_GPE_BECGroundState/main_1DGPE_v1.py
+++ results/numExp02_GPE_BECGroundState/main_1DGPE_v1.py
@@ -19,26 +19,23 @@ import numpy as np
 from SWtools import NSOM
 
 # -- SETUP AND INITIALIZATION 
-xi = np.linspace(-16, 16, 1025)
-# ... NEVP INGREDIENTS 
-cL = (0, -0.5, 0, 0)
-beta = 31.371
-F = lambda I, x: -(0.5*x**2 + beta*I)
-N0 = 1.0
-# ... INITIAL CONDITION 
-U0 = np.exp(-xi**2/2)/np.pi**0.25
-# ... NSOM INSTANTIATION
-myS = NSOM(xi, cL, F, ORP=1.5, verbose=True, nskip=10)
+xi = np.linspace(-16, 16, 1025)                 # ... COMPUTATIONAL DOMAIN
+cL = (0, -0.5, 0, 0)                            # ... LINEAR COEFFICIENTS
+beta = 31.371                                   # ... INTERACTION COEFFICIENT
+F = lambda I, x: -(0.5*x**2 + beta*I)           # ... NONLINEAR FUNCTIONAL
+N0 = 1.0                                        # ... NORMALIZATION CONSTRAINT
+U0 = np.exp(-xi**2/2)/np.pi**0.25               # ... INITIAL CONDITION
+nevp = NSOM(xi, cL, F, ORP=1.5, verbose=True)   # ... NSOM INSTANTIATION
 
 # -- NSOM SOLUTION PROCEDURE
-myS.solve(U0, N0)
+nevp.solve(U0, N0)
 
 # -- POSTPROCESSING
-U, mu = myS.U, -myS.kap
-xi_rms = np.sqrt(np.trapz((xi*U)**2,x=xi))
-E = mu - 0.5*beta*np.trapz(U**4,x=xi)
+U, mu = nevp.U, -nevp.kap                       # ... STRIP INSTANCE KEYWORD
+xi_rms = np.sqrt(np.trapz((xi*U)**2,x=xi))      # ... RMS CONDENSATE WIDTH
+E = mu - 0.5*beta*np.trapz(U**4,x=xi)           # ... CONDENSATE ENERGY 
 print(f"max(U) = {np.max(U):5.4F}")
 print(f"xi_rms = {xi_rms:5.4F}")
 print(f"E_beta = {E:5.4F}")
 print(f"mu     = {mu:5.4F}")
-print(myS); myS.show()
+print(nevp); nevp.show()
